b00a8065d9b23a0a2d502e621a8bd5b224a46eda
kvasa <kvasan2104@gmail.com>
2023-05-27 19:13:48 +0300

    changed pointers

--- main.c
+++ main.c
@@ -3,22 +3,22 @@
 #include <string.h>
 
 typedef struct Graph {
-    int **matrix;
+    int** matrix;
     int verticesNum;
 } Graph_t;
 
-char *getData(FILE *filePtr) {
+char*  getData(FILE* filePtr) {
     fseek(filePtr, 0, SEEK_END);
     int fileLen = ftell(filePtr) - 1;
     fseek(filePtr, 0, SEEK_SET);
-    char *data;
+    char* data;
     data = (char *) malloc(sizeof(char) * fileLen);
     fread(data, sizeof(char), fileLen, filePtr);
     data[fileLen] = '\0';
     return data;
 }
 
-int getVerticesNum(const char *data) {
+int getVerticesNum(const char* data) {
     int count = 0;
     int i = 0;
     while (data[i] != '\n') {
@@ -29,8 +29,8 @@ int getVerticesNum(const char *data) {
     return count;
 }
 
-Graph_t *initGraph(int verticesNum) {
-    Graph_t *graph = (Graph_t *) malloc(sizeof(Graph_t));
+Graph_t* initGraph(int verticesNum) {
+    Graph_t* graph = (Graph_t *) malloc(sizeof(Graph_t));
     graph->verticesNum = verticesNum;
     graph->matrix = (int **) malloc(verticesNum * sizeof(int *));
     for (int i = 0; i < verticesNum; i++) {
@@ -39,8 +39,8 @@ Graph_t *initGraph(int verticesNum) {
     return graph;
 }
 
-void strToGraph(Graph_t *graph, char *data) {
-    char *token;
+void strToGraph(Graph_t* graph, char* data) {
+    char* token;
     for (int i = 0; i < graph->verticesNum; i++) {
         for (int j = 0; j < graph->verticesNum; j++) {
             token = strtok_r(data, "\n ", &data);
@@ -49,7 +49,7 @@ void strToGraph(Graph_t *graph, char *data) {
     }
 }
 
-void printVerticesByDegree(Graph_t *graph, int degree) {
+void printVerticesByDegree(Graph_t* graph, int degree) {
     for (int i = 0; i < graph->verticesNum; i++) {
         int count = 0;
         for (int j = 0; j < graph->verticesNum; j++) {
@@ -60,7 +60,7 @@ void printVerticesByDegree(Graph_t *graph, int degree) {
     }
 }
 
-void freeGraph(Graph_t *graph) {
+void freeGraph(Graph_t* graph) {
     for (int i = 0; i < graph->verticesNum; i++) {
         free(graph->matrix[i]);
     }
@@ -68,29 +68,28 @@ void freeGraph(Graph_t *graph) {
     free(graph);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     if (argc != 3) {
         puts("Wrong number of arguments!");
         exit(EXIT_FAILURE);
     }
-    char *filePath = argv[1];
+    char* filePath = argv[1];
     int degree = atoi(argv[2]);
-    FILE *filePtr = fopen(filePath, "r");
+    FILE* filePtr = fopen(filePath, "r");
     if (filePtr == NULL) {
         puts("Error opening file");
         exit(EXIT_FAILURE);
     }
-    char *data = getData(filePtr);
+    char* data = getData(filePtr);
     fclose(filePtr);
     int vertices = getVerticesNum(data);
-    Graph_t *graph = initGraph(vertices);
+    Graph_t* graph = initGraph(vertices);
     strToGraph(graph, data);
     free(data);
     printVerticesByDegree(graph, degree);
     freeGraph(graph);
     return EXIT_SUCCESS;
 }
-
 
 // TODO
 // Find every vertex of a graph with a certain vertex degree
